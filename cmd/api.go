11f27005b2e5e503d1852c8ca377817fff9ef65d
kusowl <kushal.saha@outlook.com>
2026-06-13 23:25:59 +0530

    WIP: database integration
    - add docker compose for postgresql
    
    - add database configuration
    
    - initial database connection handling

--- cmd/api.go
+++ cmd/api.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5"
 )
 
 type Application struct {
@@ -29,13 +32,45 @@ func (app Application) mount() http.Handler {
 	return router
 }
 
-func (app Application) run(handler http.Handler) error {
+func (app Application) run(ctx context.Context, handler http.Handler) error {
+	app.connectDb(ctx)
 	slog.Info("Application running....")
 
 	return http.ListenAndServe(fmt.Sprintf("%s:%d", app.config.address, app.config.port), handler)
 }
 
+func (app Application) connectDb(ctx context.Context) {
+	conn, err := pgx.Connect(ctx, app.config.dbConfig.dsn())
+	if err != nil {
+		slog.Error("Database connection failed!", "error", err)
+		os.Exit(1)
+	}
+	defer conn.Close(ctx)
+}
+
 type config struct {
-	address string
-	port    int32
+	address  string
+	port     int32
+	dbConfig dbConfig
+}
+
+type dbConfig struct {
+	connection string
+	host       string
+	port       int32
+	database   string
+	username   string
+	password   string
+}
+
+func (db dbConfig) dsn() string {
+	dsn := ""
+	switch db.connection {
+	case "pgsql":
+		dsn = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", db.host, db.port, db.username, db.password, db.database)
+	default:
+		slog.Warn("Unsupported database connection type", "connection", db.connection)
+		return ""
+	}
+	return dsn
 }
--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
-	"log"
+	"context"
+	"log/slog"
+	"os"
 )
 
 func main() {
+	ctx := context.Background()
+
+	dbConfig := dbConfig{
+		connection: "pgsql",
+		host:       "localhost",
+		port:       5432,
+		database:   "postgres",
+		username:   "postgres",
+		password:   "password",
+	}
+
 	config := config{
-		address: "localhost",
-		port:    8080,
+		address:  "localhost",
+		port:     8080,
+		dbConfig: dbConfig,
 	}
 
 	app := Application{
 		config: config,
 	}
 
-	if err := app.run(app.mount()); err != nil {
-		log.Fatal("Application is shutting down to error: ", err)
+	if err := app.run(ctx, app.mount()); err != nil {
+		slog.Error("Application is shutting down to error: ", "error", err)
+		os.Exit(1)
 	}
 }
